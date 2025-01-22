710a761d42ddf1a39f7e38535546eec430f2d873
Fernando-Urbano <fernando.rocha.urbano@gmail.com>
2025-01-22 10:26:58 -0300

    added sarima and naive forecasting

--- models/naive_forecasting.py
+++ models/naive_forecasting.py
@@ -6,9 +6,9 @@ from models.time_series_model import TimeSeriesModel
 class NaiveForecasting(TimeSeriesModel):
 
     def fit(self, y, X=None):
-        self._prediction = y.iloc[:, -1].values
+        self.prediction = y.iloc[:, -1].values
 
     def forecast(self, y, X=None):
         return pd.DataFrame(
-            np.repeat(self._prediction, len(y.index)), index=y.index, columns=y.columns
+            np.repeat(self.prediction, len(y.index)), index=y.index, columns=y.columns
         )
