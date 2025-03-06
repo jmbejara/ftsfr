fc96e0c31b7bfc01f314db14379e22267e99a7cc
Jeremy Bejarano <jmbejara@gmail.com>
2025-03-06 12:56:56 -0500

    Fix wrds_bank_premium load

--- src/wrds_bank_premium/pull_wrds_bank_premium.py
+++ src/wrds_bank_premium/pull_wrds_bank_premium.py
@@ -32,6 +32,7 @@ def pull_selected_premium_tables(wrds_username=WRDS_USERNAME_BANK_PREMIUM):
     # wrds_struct_rel_ultimate.info()
 
     wrds_call_research = db.get_table(library="bank", table="wrds_call_research")
+    wrds_call_research["date"] = pd.to_datetime(wrds_call_research["date"])
     # wrds_call_research.info(verbose=True)
 
     wrds_bank_crsp_link = db.get_table(library="bank", table="wrds_bank_crsp_link")
@@ -94,13 +95,23 @@ available_tables = [
 def load_table(table_name, data_dir=DATA_DIR, subfolder=SUBFOLDER):
     if table_name not in available_tables:
         raise ValueError(f"Table {table_name} not available")
-    data_dir.mkdir(parents=True, exist_ok=True)
     return pd.read_parquet(data_dir / subfolder / f"{table_name}.parquet")
 
 
 def _demo():
-    df = load_table("wrds_struct_rel_ultimate")
-    df.info()
+    wrds_struct_rel_ultimate = load_table("wrds_struct_rel_ultimate")
+    wrds_struct_rel_ultimate.info()
+
+    wrds_call_research = load_table("wrds_call_research")
+    wrds_call_research.info(verbose=True)
+    wrds_call_research["date"] = pd.to_datetime(wrds_call_research["date"])
+    wrds_call_research[["date"]].describe()
+
+    idrssd_to_lei = load_table("idrssd_to_lei")
+    idrssd_to_lei.info()
+
+    lei_main = load_table("lei_main")
+    lei_main.info()
 
 
 if __name__ == "__main__":
