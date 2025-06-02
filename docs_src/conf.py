67dfea4a8296fb7aec94c39793baa79d2cb1d155
Jeremy Bejarano <jmbejara@gmail.com>
2025-06-02 19:27:09 -0400

    Make Sphinx pages wider

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -120,6 +120,7 @@ html_title = "Financial Time Series Forecasting Archive"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
+html_static_path = ["_static"]
 
 html_js_files = ["https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.4/require.min.js"]
+html_css_files = ["custom.css"]
