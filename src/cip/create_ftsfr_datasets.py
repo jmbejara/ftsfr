90e34f8541afa652d0a1a6510c1ec8823ee500f1
zixiaowang123 <zixiaowang@uchicago.edu>
2025-07-22 18:36:36 -0500

    Updated order of stacked time series data for CIP

--- src/cip/create_ftsfr_datasets.py
+++ src/cip/create_ftsfr_datasets.py
@@ -17,7 +17,7 @@ DATA_DIR = DATA_DIR / "cip"
 ## Calculate fx returns
 df_all = calc_cip.calculate_cip(end_date="2025-03-01", data_dir=DATA_DIR)
 df_stacked = df_all.stack().reset_index()
-df_stacked.columns = ['ds', 'unique_id', 'y']
+df_stacked.columns = ['unique_id', 'ds', 'y']
 
 df_stacked.to_parquet(DATA_DIR / "ftsfr_CIP_spreads.parquet")
 
