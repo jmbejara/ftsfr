81f6d6f325657379d5563b9b4a52156d303c839d
Jeremy Bejarano <jmbejara@gmail.com>
2025-07-31 11:49:45 -0400

    Debug help with treasuries

--- src/us_treasury_returns/calc_treasury_bond_returns.py
+++ src/us_treasury_returns/calc_treasury_bond_returns.py
@@ -14,6 +14,7 @@ from settings import config
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 DATA_DIR = config("DATA_DIR")
+# DATA_DIR = DATA_DIR / "us_treasury_returns"
 
 
 def group_portfolios(bond_returns=None):
